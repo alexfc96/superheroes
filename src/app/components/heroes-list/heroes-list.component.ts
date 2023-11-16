81365bcf22be762bd22d539edea491f3837fdc43
Alex Fernandez <alex.fernandez.sge@mango.com>
2023-11-16 12:50:37 +0100

    feat: implemented search input to search by name

--- src/app/components/heroes-list/heroes-list.component.ts
+++ src/app/components/heroes-list/heroes-list.component.ts
@@ -3,17 +3,22 @@ import { NgOptimizedImage } from '@angular/common'
 import { Hero } from '../../heroes';
 import { SuperHeroesService } from '../../services/service';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'heroes-list',
   standalone: true,
-  imports: [MatCardModule, NgOptimizedImage],
+  imports: [MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule ,NgOptimizedImage, FormsModule],
   templateUrl: './heroes-list.component.html',
   styleUrls: ['./heroes-list.component.css']
 })
 export class HeroesListComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero: Hero | undefined;
+  searchHeroName: string = '';
 
   constructor(private service: SuperHeroesService) {}
 
@@ -25,8 +30,7 @@ export class HeroesListComponent implements OnInit {
     this.heroes = this.service.getHeroes();
   }
 
-  onFilterName(name: string) {
+  onFilterChange(name: string) {
     this.heroes = this.service.getHeroesByName(name);
   }
-
 }
